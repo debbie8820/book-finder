47b4a4b5dbe57334d18cc1b28d12713aa5d1a05f
Debbie <debbiecamera0704@gmail.com>
2021-08-13 16:03:46 +0800

    adjust env variable names and remove unused codes

--- config/sendEmail.js
+++ config/sendEmail.js
@@ -2,17 +2,17 @@ const nodemailer = require('nodemailer')
 const transporter = nodemailer.createTransport({
   service: 'Hotmail',
   auth: {
-    user: process.env.hotmailName,
-    pass: process.env.hotmailPass
+    user: process.env.hotmailEmail,
+    pass: process.env.hotmailPassword
   }
 })
 
 
 module.exports = function (message) {
   const options = {
-    to: process.env.to,
-    from: process.env.from,
-    subject: process.env.subject,
+    to: process.env.nodemailer_send_to,
+    from: process.env.nodemailer_send_from,
+    subject: process.env.nodemailer_subject,
     text: `資料庫發出的最新通知如下: ${message}`
   }
   transporter.sendMail(options, (err, info) => {
