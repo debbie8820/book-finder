e2e27c85b0c3bab6b13ca09d7a5d2aed8ce57f9e
Debbie <debbiecamera0704@gmail.com>
2021-08-16 03:10:41 +0800

    fix errors in scrapeBooksCITE.js

--- config/scrapeBooksCITE.js
+++ config/scrapeBooksCITE.js
@@ -26,6 +26,7 @@ module.exports = async (URL, keyword) => {
     for (let i = 0; i < allLists.length; i++) {
       const allLists = CITE$('.book-container li.book-area-1')
       const name = allLists.eq(i).find('div.book-info-1 h2 a').text()
+      const author = allLists.eq(i).find('div.book-info-1 span.font-color-990 a').eq(0).text()
       if (regex.test(name) || regex.test(author)) {
         const img = allLists.eq(i).find('div.book-img').children().eq(0).find('img').attr('src')
         const allPrices = allLists.eq(i).find('div.book-info-2 span.font-color01').length //區分折扣與價格
@@ -35,7 +36,6 @@ module.exports = async (URL, keyword) => {
           discount = allLists.eq(i).find('div.book-info-2 span.font-color01').eq(0).text()
           price = allLists.eq(i).find('div.book-info-2 span.font-color01').eq(1).text()
         }
-        const author = allLists.eq(i).find('div.book-info-1 span.font-color-990 a').eq(0).text()
         const stock = allLists.eq(i).find('div.book-info-2 div.button02').html() ? 1 : 0
         const productNumber = allLists.eq(i).find('div.book-info-1 a').attr('href').slice(9)
         const url = allLists.eq(i).find('div.book-info-1 div.padding-top-10px a').attr('href')
